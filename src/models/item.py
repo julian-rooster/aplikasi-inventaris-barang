a5312c7f4e8950ba80efa607a8d07bf92de0e8b5
Agnaar <agnaar218@gmail.com>
2026-03-03 21:41:23 +0700

    Menambahkan method total_value dan __str__ pada class Item

--- src/models/item.py
+++ src/models/item.py
@@ -1,17 +1,14 @@
 class Item:
     def __init__(self, name, quantity, price):
 
-        # Validasi name
         if not name or not isinstance(name, str):
             raise ValueError("Nama barang harus berupa teks dan tidak boleh kosong")
 
-        # Validasi quantity
         if not isinstance(quantity, int):
             raise TypeError("Quantity harus berupa angka bulat")
         if quantity < 0:
             raise ValueError("Quantity tidak boleh negatif")
 
-        # Validasi price
         if not isinstance(price, (int, float)):
             raise TypeError("Price harus berupa angka")
         if price < 0:
@@ -20,3 +17,9 @@ class Item:
         self.name = name
         self.quantity = quantity
         self.price = price
+
+    def total_value(self):
+        return self.quantity * self.price
+
+    def __str__(self):
+        return f"{self.name} | Stok: {self.quantity} | Harga: {self.price}"
